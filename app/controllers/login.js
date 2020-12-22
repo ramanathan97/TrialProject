ba74831d1d88ecce224d8c2868d228ff0251be99
ramanathan97 <52155584+ramanathan97@users.noreply.github.com>
2020-12-22 15:28:50 +0530

    Update login.js

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,24 +2,7 @@ import Controller from '@ember/controller';
 
 
 export default Controller.extend({
-    actions:{
-        checkUser: function() {
-            let currentmail = this.get('email');
-            let currentpassword = this.get('password');
-
-             let persons = this.store.findAll('user');
-             persons.then(function(value){
-                 value.forEach(element=>{
-                     if(element.email==currentmail&&element.password==currentpassword){
-                         window.location = "http://localhost:4200/performance"
-
-                     }
-                     
-                 })
-               }) 
-             
-        }
-    }
+    
     
    
 });
