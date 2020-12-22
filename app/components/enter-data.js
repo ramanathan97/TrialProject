995219ef2c4bf7c172128dd53c09e076ed523539
ramanathan97 <52155584+ramanathan97@users.noreply.github.com>
2020-12-22 17:53:40 +0530

    Update enter-data.js

--- app/components/enter-data.js
+++ app/components/enter-data.js
@@ -1,24 +1,14 @@
 import Component from '@ember/component';
 import { inject as service } from '@ember/service';
-
 export default Component.extend({
-    
     store: service(),
-
-    
-      
-      actions:{
-       submitData: function(){
-           let distance=this.get('distance');
-           let duration=this.get('duration');
-           let performance = this.store.createRecord('performance', {
-               distance:distance,
-               duration:duration,
-                          
-          });
-    
-       }
-
-      }
-
+    actions: {
+        submitData () {
+            let performance = this.store.createRecord('performance', {
+                distance: this.get('distance'),
+                duration: this.get('duration'),
+            });
+            performance.save();
+        }
+    }
 });
