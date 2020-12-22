dabe62a356c678be962f599172f347c211240da0
ramanathan97 <52155584+ramanathan97@users.noreply.github.com>
2020-12-22 15:13:51 +0530

    Update ranking.js

--- app/routes/ranking.js
+++ app/routes/ranking.js
@@ -1,4 +1,45 @@
 import Route from '@ember/routing/route';
-
 export default Route.extend({
+    display:'',
+    rankCity:false,
+    displayPerformance:false,
+    currentcity:'',
+    performances:[],
+    beforeModel(){
+        let mail=localStorage.getItem("email");
+        if(mail==undefined){
+        this.transitionTo('login')
+    }        
+    },
+    model(){
+        return this.store.query('performance', { include: 'user' })
+    },
+    setupController(controller, model) {
+        this._super(controller, model);
+        controller.set('actions', {
+            calculateRank: function(arg1){
+                let type=arg1;                
+                if(type=="distance")
+                {
+                    this.set('display','rankDistance')
+                    this.set('rankCity',false)
+                    this.set('displayPerformance',false)
+                }   
+                if(type=="duration")
+                {
+                    this.set('display','rankDuration')
+                    this.set('rankCity',false)
+                    this.set('displayPerformance',false)
+                }      
+            },
+            sortByCity:function(){
+                this.set('currentcity',this.get("city"));
+                this.set('displayPerformance',true)
+            },
+            calcCity(){
+                this.set('display','rankCity')
+                this.set('rankCity',true)
+            }   
+        })
+    }
 });
