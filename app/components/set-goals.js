500dba9af1f146b8c8c49e9a791e9c3c3e5f8bba
ramanathan97 <52155584+ramanathan97@users.noreply.github.com>
2020-12-24 13:37:53 +0530

    Update set-goals.js

--- app/components/set-goals.js
+++ app/components/set-goals.js
@@ -3,12 +3,14 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
     store: service(),
     actions: {
-        submitData() {
-            let goal = this.store.createRecord('goal', {
-                distanceGoal: this.get('distancegoal'),
-                durationGoal: this.get('durationgoal'),
-            });
-            goal.save();
-        }
+        submitData(durationgoal,distancegoal) {
+            this.store.createRecord('goal', {
+                distanceGoal:distancegoal,
+                durationGoal:durationgoal,
+            }).save();         
+            if(durationgoal<0||distancegoal<0){
+                toastr.error('Please enter valid goals');
+            }
+        }       
     }
 });
