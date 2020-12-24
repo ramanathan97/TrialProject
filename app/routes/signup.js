6cc6afb154c77ae9155380e5322a824334c366a9
ramanathan97 <52155584+ramanathan97@users.noreply.github.com>
2020-12-24 13:31:42 +0530

    Update signup.js

--- app/routes/signup.js
+++ app/routes/signup.js
@@ -1,39 +1,28 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-
 export default Route.extend({
     ajax: service(),
     setupController(controller, model) {
         this._super(controller, model);
-        let route = this;
         controller.set('actions', {
-            createUser: function() {
-                let val;           
-               let person = this.store.createRecord('user', {
-                 name:this.get('name'),
-                 email: this.get('email'),
-                 age: this.get('age'),
-                 city: this.get('city'),
-                 state: this.get('state'),
-                 country:this.get('country'),
-                 password:  this.get('password')
-               });
-              val= person.save();
-              
-            val.then(value => {
-                  console.log(value)
-                  if (value == true){
-                      this.transitionToRoute('login');
-                  }
-                  else
-                  toastr.error('Some Error occured')
-              }
-              )
-              },
-
-            
+            createUser: function (name,age,email,city,state,country,password) {
+                this.store.createRecord('user', {
+                    name: name,
+                    email: email,
+                    age: age,
+                    city: city,
+                    state: state,
+                    country: country,
+                    password: password
+                }).save().then(signupresponse => {
+                    if (signupresponse === true) {
+                        this.transitionToRoute('login');
+                    } else {
+                        toastr.error('Some Error occured');
+                    }
+                });
+            },
         })
     }
 });
-
