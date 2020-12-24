4ef2f586da6aa7c56944ffb9b080fb2cfafb0fe4
ramanathan97 <52155584+ramanathan97@users.noreply.github.com>
2020-12-24 13:12:42 +0530

    Update login.js

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,34 +1,33 @@
-import Route from '@ember/routing/route';
+iimport Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+
 export default Route.extend({
     ajax: service(),
     setupController(controller, model) {
         this._super(controller, model);
         let route = this;
-        let val = '';
-        controller.set("email", '')
-        controller.set("password", '')
+        controller.set('email', '');
+        controller.set('password', '');
         controller.set('actions', {
             userCheck() {
-                val = route.get('ajax').request('/login', {
+                 route.get('ajax').request('/login', {
                     method: 'POST',
                     data: JSON.stringify({
                         username: controller.get('email'),
                         password: controller.get("password")
                     }),
-                })
-                val.then(value => {
-                  localStorage.setItem('servervalue',value)
-                    if (value == true){
+                  }).then(loginpermit => {
+                  localStorage.setItem('loginpermit', loginpermit);
+                    if (loginpermit === true) {
                         this.transitionToRoute('performance');
+                    } else {
+                     toastr.error('Please enter correct user name and password');
                     }
-                    else
-                    toastr.error('Please enter correct user name and password')
-                }
-                )
+                })
             }
         })
     },
 });
+
 
 
