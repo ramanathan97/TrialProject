33f1e3dfaf4f1d425fb3a7ee82583c049b2aa9a2
ramanathan97 <52155584+ramanathan97@users.noreply.github.com>
2020-12-29 10:37:53 +0530

    Update enter-data.js

--- app/components/enter-data.js
+++ app/components/enter-data.js
@@ -4,12 +4,13 @@ export default Component.extend({
     store: service(),
     actions: {
         submitData (duration,distance) {
-            this.store.createRecord('performance', {
-                distance: distance,
-                duration: duration,
-            }).save();
             if(duration<0||distance<0){
                 toastr.error('Please enter valid data');
+            } else{
+                this.store.createRecord('performance', {
+                    distance: distance,
+                    duration: duration,
+                }).save();  
             }
         }
     }
